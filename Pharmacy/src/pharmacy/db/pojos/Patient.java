852e9fffdb63054e572a09cbe0f0ec81f935cad9
MartinaZandio <martina.zandiolizarraga@usp.ceu.es>
2024-04-16 16:47:52 +0200

    POJO patient created.

--- Pharmacy/src/pharmacy/db/pojos/Patient.java
+++ Pharmacy/src/pharmacy/db/pojos/Patient.java
@@ -1,5 +1,79 @@
 package pharmacy.db.pojos;
 
-public class Patient {
+import java.io.Serializable;
+import java.util.Objects;
 
+public class Patient implements Serializable{
+	
+	private Integer id;
+	private String name;
+	private String dateOfBirth;
+	private enum gender{MALE, FEMALE}
+	private gender sex;
+	
+	public Patient(Integer id, String name, String dateOfBirth, gender sex) {
+		super();
+		this.id=id;
+		this.name=name;
+		this.dateOfBirth=dateOfBirth;
+		this.sex=gender.MALE;
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public gender getSex() {
+		return sex;
+	}
+
+	public void setSex(gender sex) {
+		this.sex = sex;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(dateOfBirth, id, name, sex);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Patient other = (Patient) obj;
+		return Objects.equals(dateOfBirth, other.dateOfBirth) && Objects.equals(id, other.id)
+				&& Objects.equals(name, other.name) && sex == other.sex;
+	}
+
+	@Override
+	public String toString() {
+		return "Patient [id=" + id + ", name=" + name + ", dateOfBirth=" + dateOfBirth + ", sex=" + sex + "]";
+	}
+	
+	
 }
