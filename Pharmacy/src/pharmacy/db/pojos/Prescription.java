97058cfabe4d38d545aad5cde6a4936d46882e2a
blancaamaldonadoo <blanca.maldonadoperezhidalgo@usp.ceu.es>
2024-04-16 16:50:17 +0200

    commit

--- Pharmacy/src/pharmacy/db/pojos/Prescription.java
+++ Pharmacy/src/pharmacy/db/pojos/Prescription.java
@@ -1,5 +1,70 @@
 package pharmacy.db.pojos;
 
+import java.util.Objects;
+
 public class Prescription {
 
+	private int quantity;
+	private String issueDate;
+	private String dateUsed;
+	
+	public Prescription(int quantity, String issueDate, String dateUsed){
+		
+		this.quantity=quantity;
+		this.issueDate=issueDate;
+		this.dateUsed=dateUsed;
+		
+	}
+	
+	//Getters y Setters
+	
+	public int getQuantity() {
+		return quantity;
+	}
+	
+	public void setQuantity(int quantity) {
+		this.quantity=quantity;
+	}
+	
+	public String getIssueDate(){
+		return issueDate;
+	}
+	
+	public void setIssueDate(String issueDate) {
+		this.issueDate=issueDate;
+	}
+	
+	public String getDateUsed(){
+		return dateUsed;
+	}
+	
+	public void setDateUsed(String dateUsed) {
+		this.dateUsed=dateUsed;
+	}
+
+	@Override
+	public String toString() {
+		return "Prescription [quantity=" + quantity + ", issueDate=" + issueDate + ", dateUsed=" + dateUsed + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(dateUsed, issueDate, quantity);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Prescription other = (Prescription) obj;
+		return Objects.equals(dateUsed, other.dateUsed) && Objects.equals(issueDate, other.issueDate)
+				&& quantity == other.quantity;
+	}
+	
+	
+	
 }
