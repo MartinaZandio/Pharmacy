8446adff5cb4a1f2ad1869a1c9703af998016b53
MartinaZandio <martina.zandiolizarraga@usp.ceu.es>
2024-04-29 18:28:52 +0200

    cambios.

--- Pharmacy/src/pharmacy/db/pojos/Medicine.java
+++ Pharmacy/src/pharmacy/db/pojos/Medicine.java
@@ -1,5 +1,6 @@
 package pharmacy.db.pojos;
 
+import java.util.ArrayList;
 import java.util.Objects;
 import java.io.Serializable;
 
@@ -9,18 +10,26 @@ public class Medicine implements Serializable {
 	private int numAsigned;
 	private Prescription prescription;
 	private Laboratory laboratory;
-
+	private ArrayList<Stock> stock;
+	
 	public Medicine() {
 		super();
 	}
 	
-	public Medicine(String name, int numAsigned){
 	
-		this.name= name;
-		this.numAsigned=numAsigned;
-		
+	
+	public Medicine(String name, int numAsigned, Prescription prescription, Laboratory laboratory,
+			ArrayList<Stock> stock) {
+		super();
+		this.name = name;
+		this.numAsigned = numAsigned;
+		this.prescription = prescription;
+		this.laboratory = laboratory;
+		this.stock = stock;
 	}
-	
+
+
+
 	//Getters y Setters
 	
 	public String getName() {
@@ -55,9 +64,17 @@ public class Medicine implements Serializable {
 		this.laboratory = laboratory;
 	}
 
+	public ArrayList<Stock> getStock() {
+		return stock;
+	}
+
+	public void setStock(ArrayList<Stock> stock) {
+		this.stock = stock;
+	}
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(laboratory, name, numAsigned, prescription);
+		return Objects.hash(laboratory, name, numAsigned, prescription, stock);
 	}
 
 	@Override
@@ -70,15 +87,17 @@ public class Medicine implements Serializable {
 			return false;
 		Medicine other = (Medicine) obj;
 		return Objects.equals(laboratory, other.laboratory) && Objects.equals(name, other.name)
-				&& numAsigned == other.numAsigned && Objects.equals(prescription, other.prescription);
+				&& numAsigned == other.numAsigned && Objects.equals(prescription, other.prescription)
+				&& Objects.equals(stock, other.stock);
 	}
 
 	@Override
 	public String toString() {
 		return "Medicine [name=" + name + ", numAsigned=" + numAsigned + ", prescription=" + prescription
-				+ ", laboratory=" + laboratory + "]";
+				+ ", laboratory=" + laboratory + ", stock=" + stock + "]";
 	}
 
+	
 	
 	
 	
