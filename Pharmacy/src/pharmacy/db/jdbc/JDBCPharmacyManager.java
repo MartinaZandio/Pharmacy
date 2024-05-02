fdd3dc35a99787832419941d761e34b0e7d8578f
gabrielamendez <gabrielamendez@localhost>
2024-05-02 21:02:25 +0200

    JDBCPharmacyManager changes in the methods

--- Pharmacy/src/pharmacy/db/jdbc/JDBCPharmacyManager.java
+++ Pharmacy/src/pharmacy/db/jdbc/JDBCPharmacyManager.java
@@ -1,7 +1,11 @@
 package pharmacy.db.jdbc;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
@@ -13,10 +17,13 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import library.db.pojos.Author;
 import pharmacy.db.interfaces.PharmacyManager;
 import pharmacy.db.pojos.*;
 import pharmacy.db.pojos.Patient.gender;
+import sample.db.graphics.ImageWindow;
 import sample.db.pojos.Department;
+import sample.db.pojos.Employee;
 
 
 public class JDBCPharmacyManager implements PharmacyManager {
@@ -38,25 +45,25 @@ public class JDBCPharmacyManager implements PharmacyManager {
 
 	@Override
 	public void markPrescriptionAsUsed(Prescription prescription) {
-		// TODO Auto-generated method stub
 		try {
-			// Change a department's location: beginning
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 			System.out.println("Choose a prescription by typing its ID: ");
 			// Show all id's method??
-			int presc_id = Integer.parseInt(reader.readLine());
-			
-			// useDate = CurrentDate
-			LocalDate useDate; 
-		
-			
-		
+			int dep_id = Integer.parseInt(reader.readLine());
+			System.out.println("Choose by typing one option: USED / NOT USED");
+			String usage = reader.readLine();
+			String sql = "UPDATE prescriptions WHERE id LIKE ? SET used=?";
+			PreparedStatement p = c.prepareStatement(sql);
+			p.setInt(1, dep_id);
+			p.setString(2, usage);
+			p.executeUpdate();
 			System.out.println("Update finished.");
-
+			c.close();
 		} catch (Exception e) {
 			System.out.println("Error modifying prescription.");
 		}
 	}
+	
 
 	@Override
 	public void checkAuthenticity(Prescription prescription) {
@@ -72,35 +79,40 @@ public class JDBCPharmacyManager implements PharmacyManager {
 
 	@Override
 	public void orderStock(Medicine medicine) {
-<<<<<<< HEAD
 		// TODO Auto-generated method stub
-	
-=======
 
-		
->>>>>>> branch 'master' of https://github.com/MartinaZandio/Pharmacy
 	}
 
 	@Override
 	public void identifyPatient(Patient patient) {
-		// TODO Auto-generated method stub
 		try {
-			String template = "SELECT * FROM patients WHERE name LIKE ?";
-			PreparedStatement search = c.prepareStatement(template);
-			search.setString(1, "%" + name + "%");
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Type the patients name: ");
+			String name = reader.readLine();
+			String sql = "SELECT * FROM patients WHERE name LIKE ?";
+			PreparedStatement search = c.prepareStatement(sql);
+			search.setString(1, name);
 			ResultSet rs = search.executeQuery();
 			while(rs.next()) {
 				Integer id = rs.getInt("id");
-				String name = rs.getString("name");
+				String name1 = rs.getString("name");
 				Date dateOfBirth = rs.getDate("dateOfBirth");
 				gender sex = rs.getSex("sex");
-				}
+				Patient p = new Patient(id, name, dateOfBirth, sex);
+				System.out.println(p);
+			}
+			rs.close();
+			search.close();
+			System.out.println("Search finished.");
+			c.close();
+			System.out.println("Database connection closed.");
 			} catch (SQLException e) {
 				System.out.println("Error creating the prescription");
 				e.printStackTrace();
 				}
 		}
-
+	
+	
 	@Override
 	public void assignMedicine(Medicine medicine) {
 		// TODO Auto-generated method stub
@@ -109,9 +121,21 @@ public class JDBCPharmacyManager implements PharmacyManager {
 
 	@Override
 	public void checkStock(Medicine medicine) {
-		// TODO Auto-generated method stub
-		
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Type the medicine's id: ");
+			// Show medicines id 
+			int medicine_id = Integer.parseInt(reader.readLine());
+			String sql = "SELECT name, id, stock FROM medicine WHERE id LIKE ?";
+			PreparedStatement search = c.prepareStatement(sql); 
+			search.setInt(1, medicine_id);
+			search.close();
+			System.out.println("Search finished.");
+			c.close();
+			System.out.println("Database connection closed.");
+		} catch (SQLException e) {
+			System.out.println("Error creating the prescription");
+			e.printStackTrace();
+			}
+		}
 	}
-
-
-}
