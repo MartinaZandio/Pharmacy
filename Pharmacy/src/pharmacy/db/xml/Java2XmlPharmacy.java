9d8e95944c8658c5b949773bba2d4941b6e7356d
SaraMenor <sara.menortorrero@usp.ceu.es>
2024-05-16 17:47:56 +0200

    M

--- Pharmacy/src/pharmacy/db/xml/Java2XmlPharmacy.java
+++ Pharmacy/src/pharmacy/db/xml/Java2XmlPharmacy.java
@@ -14,69 +14,18 @@ import javax.persistence.*;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
 
-import library.db.pojos.Author;
-import library.db.pojos.Book;
 import pharmacy.db.jdbc.ConnectionManager;
 import pharmacy.db.pojos.*;
 
 public class Java2XmlPharmacy {
 
 	
-	private static Connection c; 
-	private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-	private ConnectionManager conMan; 
-	
-	public Java2XmlPharmacy(ConnectionManager conMan) {
-		this.conMan = conMan; 
-		this.c = conMan.getConnection();
-	}
-	
-	private static void printPharmacies() {
-	
-		String sql = "SELECT * FROM pharmacies";
-		PreparedStatement prep= c.prepareStatement(sql);
-		ResultSet rs = prep.executeQuery();
-		rs.next();
-		while(rs.next()) {
-			Integer id = rs.getInt("id");
-			String name = rs.getString("name");
-			String location=rs.getString("location");
-			Integer postalCode = rs.getInt("postalCode");
-			Integer numberOfWorkers=rs.getInt("numberOfWorkers");
-			Medicine medicine= new Medicine(rs.getString("name"), rs.getInt("numAssigned")); 
-			Stock stock= new Stock(rs.getInt("amount"));
-			Pharmacy ph= new Pharmacy(id, name, location, postalCode, numberOfWorkers,???????);
-		}
-		
-			
-	}
-	
-	public static void main (String[]args ) throws Exception{
-		String url = "jdbc:sqlite:pharmacy-provider.db";
-		try {
-			Connection c = DriverManager.getConnection(url);
-			c.setAutoCommit(false);
-			java.sql.Statement stmt=c.createStatement();
-			stmt.executeUpdate("PRAGMA foreign_keys=ON");
-		   c.commit();
-			c.close();
-		}catch (SQLException e) {
-		}
-
-	
 		JAXBContext jaxbContext = JAXBContext.newInstance(Pharmacy.class);
 		Marshaller marshaller = jaxbContext.createMarshaller();
+		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 		
-		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT,Boolean.TRUE);
 		
-		printPharmacies();
-		System.out.print("Choose a pharmacy to turn into an XML file:");
-		int id=Integer.parseInt(reader.readLine());
-		PreparedStatement prep2= c.prepareStatement("SELECT * FROM reports WHERE id= ?");
-		prep2.setInt(1, id);
-		ResultSet rs = prep2.executeQuery();
-		rs.next();
-		Pharmacy pharmacy = (Pharmacy) prep2.getResultSet();
+		 // Pharmacy pharmacy you got from somewhere;
 		
 		File file = new File("./xmls/Sample-Pharmacy.xml");
 		marshaller.marshal(pharmacy,file);
@@ -84,4 +33,3 @@ public class Java2XmlPharmacy {
 		
 	}
 	
-}
